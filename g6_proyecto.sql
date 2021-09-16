fd99a3fafddf4e86cbd381025ff807850ed059a4
andrees971 <andres.silvamunoz@campusucc.edu.co>
2021-09-16 13:17:57 -0500

    se modifico el archivo g6_proyecto.sql, se agrego la tabla categoria y se añadio la relacion con restaurantes y productos

--- g6_proyecto.sql
+++ g6_proyecto.sql
@@ -2,10 +2,13 @@ CREATE DATABASE if NOT EXISTS g6_proyecto;
 
 USE g6_proyecto;
 
-DROP TABLE if EXISTS tipoRestaurante;
+
 DROP TABLE if EXISTS restaurante_producto;
+DROP TABLE if EXISTS restaurante_categoria;
 DROP TABLE if EXISTS restaurante;
 DROP TABLE if EXISTS producto;
+DROP TABLE if EXISTS categoria;
+
 
 
 CREATE TABLE if NOT EXISTS restaurante(
@@ -17,18 +20,26 @@ direccion VARCHAR(30) NOT NULL
 );
 
 
-CREATE TABLE if NOT EXISTS tipoRestaurante(
-idTipoRestaurante INT PRIMARY KEY,
-nombreTpRestaurante VARCHAR(25) NOT NULL,
+CREATE TABLE if NOT EXISTS categoria(
+idCategoria INT PRIMARY KEY,
+nombreCategoria VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE if NOT EXISTS restaurante_categoria(
+idRC INT PRIMARY KEY,
 idRestaurante INT,
-CONSTRAINT fk_restaurante_tp FOREIGN KEY(idRestaurante) REFERENCES restaurante(idRestaurante)
+idCategoria INT,
+CONSTRAINT fk_rest_cate FOREIGN KEY(idRestaurante) REFERENCES restaurante(idRestaurante),
+CONSTRAINT fk_cate_rest FOREIGN KEY(idCategoria) REFERENCES categoria(idCategoria)
 );
 
 
 CREATE TABLE if NOT EXISTS producto(
 idProducto INT PRIMARY KEY,
 nombre VARCHAR(25) NOT NULL,
-categoria VARCHAR(25) NOT NULL
+categoria VARCHAR(25) NOT NULL,
+idCategoria INT,
+CONSTRAINT fk_cate_prod FOREIGN KEY(idCategoria) REFERENCES categoria(idCategoria)
 );
 
 
@@ -38,7 +49,7 @@ precioProducto INT NOT NULL,
 descripcionProdcuto VARCHAR(100) NOT NULL,
 idRestaurante INT,
 idProducto INT,
-CONSTRAINT fk_restaurante_rp FOREIGN KEY(idRestaurante) REFERENCES restaurante(idRestaurante),
-CONSTRAINT fk_prodcuto_rp FOREIGN KEY(idProducto) REFERENCES producto(idProducto)
+CONSTRAINT fk_rest_prod FOREIGN KEY(idRestaurante) REFERENCES restaurante(idRestaurante),
+CONSTRAINT fk_prod_rest FOREIGN KEY(idProducto) REFERENCES producto(idProducto)
 );
 
